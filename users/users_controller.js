34b976e0427bdcbc1952a2f774401efe4a0dbb37
Jose Mateus <jmateussilva044@gmail.com>
2025-01-27 19:11:44 -0300

    feat: cadastro de usuários implementado

--- users/users_controller.js
+++ users/users_controller.js
@@ -1,11 +1,42 @@
 const express = require("express")
 const User = require("./User")
-const { Op } = require("sequelize");
+const { Op } = require("sequelize")
+const bcrypt = require("bcryptjs")
 const router = express.Router()
 
 
 router.get("/users/create", (request, response) => {
     response.render("admin/users/create_user")
+})
+
+router.post("/user/save", (request, response) => {
+    var username = request.body.name
+    var email = request.body.email
+    var password = request.body.password
+    User.findOne({
+        where: {
+            email: email
+        }
+    }).then(user => {
+        if(user != undefined) {
+            response.redirect("/users/create")
+        }
+        else 
+        {
+            var salt = bcrypt.genSaltSync(10)
+            console.log(salt)
+            var hash = bcrypt.hashSync(password, salt)
+            User.create({
+                username: username,
+                email: email,
+                password: hash
+            }).then(() => {
+                response.redirect("/")
+            }).catch((err) => {
+                response.redirect("/users/create")
+            })
+        }
+    }).catch()
 })
 
 module.exports = router
